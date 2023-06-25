5186ba1f418bbdde1d912dd1c388ff4b31e30a58
phucnguyen02 <ndhp02@gmail.com>
2023-06-24 20:31:42 -0700

    Sequelize changes

--- server/index.js
+++ server/index.js
@@ -121,6 +121,7 @@ io.on('connection', (socket) => {
     })
 })
 
-db.sequelize.sync().then((req) => {
+db.sequelize.sync({ alter: true, logging: false }).then((req) => {
     server.listen(4000, () => 'Server is running on port 4000')
 })
+
